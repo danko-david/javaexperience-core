72eee315fd39e06f38f12a7c985a08ac9e96b0aa
Dankó Dávid <info@dankodavid.hu>
2019-02-26 15:32:59 +0100

    end set default loglevel

--- src/main/java/eu/javaexperience/log/JavaExperienceLoggingFacility.java
+++ src/main/java/eu/javaexperience/log/JavaExperienceLoggingFacility.java
@@ -14,7 +14,9 @@ import java.util.Set;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 
+import eu.javaexperience.arrays.ArrayTools;
 import eu.javaexperience.collection.PublisherCollection;
+import eu.javaexperience.collection.enumerations.EnumTools;
 import eu.javaexperience.io.IOTools;
 import eu.javaexperience.io.LocklessPrintWriter;
 import eu.javaexperience.parse.ParsePrimitive;
@@ -31,6 +33,20 @@ public class JavaExperienceLoggingFacility
 	
 	protected static LoggingDetailLevel lookupDefaultLoglevel()
 	{
+		String ll = System.getenv("JVX_DEFAULT_LOG_LEVEL");
+		if(null != ll)
+		{
+			LogLevel ret = EnumTools.recogniseSymbol(LogLevel.class, ll);
+			if(null == ret)
+			{
+				System.out.println("Env variable `JVX_DEFAULT_LOG_LEVEL` specified as `"+ll+"` which is an unrecognisable loglevel. Available loglevels are: "+ArrayTools.toString(LogLevel.values()));
+			}
+			else
+			{
+				return ret;
+			}
+		}
+		
 		return LogLevel.MEASURE;
 	}
 	
