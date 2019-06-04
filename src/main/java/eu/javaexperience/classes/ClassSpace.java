e36eead82e037365d9baf7a2d2f769730d080cec
Dankó Dávid <info@dankodavid.hu>
2019-06-04 17:20:35 +0200

    - add more ClassSpace function to implement

--- src/main/java/eu/javaexperience/classes/ClassSpace.java
+++ src/main/java/eu/javaexperience/classes/ClassSpace.java
@@ -1,6 +1,11 @@
 package eu.javaexperience.classes;
 
+import java.util.List;
+
 public interface ClassSpace
 {
+	public List<ClassDescriptor> getLoadedClasses();
 	public ClassDescriptor getClassByName(String name);
+	public boolean registerClass(String name, ClassDescriptor classDescriptor);
+	public boolean removeClass(String name);
 }
