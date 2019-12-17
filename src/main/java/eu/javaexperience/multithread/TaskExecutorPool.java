6ccbfb5b20a275753cf2a155895f9f15116e58a9
Dankó Dávid <info@dankodavid.hu>
2019-12-17 01:09:09 +0100

    Add the old good TaskExecutorPool from the old library. Something is weird with the ThreadPoolExecutor, this is a simple unlimited version of that.

---
+++ src/main/java/eu/javaexperience/multithread/TaskExecutorPool.java
@@ -0,0 +1,37 @@
+package eu.javaexperience.multithread;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.Executor;
+
+public abstract class TaskExecutorPool implements Executor
+{
+	protected final Collection<RerunnableThread<Runnable>> execs = new ArrayList<>();
+	
+	protected RerunnableThread<Runnable> executorCreator()
+	{
+		return new RerunnableThread<Runnable>()
+		{
+			@Override
+			public void runThis(Runnable param) throws Throwable
+			{
+				param.run();
+			}
+		};
+	}
+	
+	public synchronized void execute(Runnable exec)
+	{
+		for(RerunnableThread<Runnable> e:execs)
+		{
+			if(e.rerunIfFreeWithParam(exec))
+			{
+				return;
+			}
+		}
+		
+		RerunnableThread<Runnable> r = executorCreator();
+		r.rerunWithParam(exec);
+		execs.add(r);
+	}
+}
